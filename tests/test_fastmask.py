64b69a9ef46ae384cb2fc68b018262dbf8bdb226
Andrew Luzan <luzan.minsk@gmail.com>
2024-04-26 18:49:12 +0300

    add magic bytes/version tests

--- tests/test_fastmask.py
+++ tests/test_fastmask.py
@@ -10,11 +10,11 @@ class TestFastMask(unittest.TestCase):
 
     def assert_image_same_after_read_write(self, img, name):
         # Write the image
-        pf.write("test.bin", img)
-        r = pf.read("test.bin")
+        pf.write("test.pfm", img)
+        r = pf.read("test.pfm")
 
         # remove test file
-        os.remove("test.bin")
+        os.remove("test.pfm")
 
         # Check the image
         np.testing.assert_array_equal(img, r, err_msg=f"Mask {name} is not equal to the read mask.")
@@ -37,3 +37,28 @@ class TestFastMask(unittest.TestCase):
     def test_noise_uint8(self):
         mask = np.random.randint(0, 256, (100, 100), dtype=np.uint8)
         self.assert_image_same_after_read_write(mask, 'noise')
+
+    def test_magic_bytes(self):
+        mask = np.random.randint(0, 256, (100, 100), dtype=np.uint8)
+        pf.write("test.bin", mask)
+        with open("test.bin", "rb") as f:
+            self.assertEqual(f.read(4), b'pfmf')
+        os.remove("test.bin")
+
+    def test_read_wrong_file_produces_error(self):
+        with open("test.pfm", "wb") as f:
+            f.write(b'wrong')
+
+        with self.assertRaises(ValueError):
+            pf.read("test.pfm")
+
+        os.remove("test.pfm")
+
+    def test_read_wrong_version_produces_error(self):
+        with open("test.pfm", "wb") as f:
+            f.write(b'pfmf\x99')
+
+        with self.assertRaises(ValueError):
+            pf.read("test.pfm")
+
+        os.remove("test.pfm")
