18787be4f2f4aab31757c8ce97a9b071e8aba3b3
Andrew Luzan <luzan.minsk@gmail.com>
2024-05-25 21:42:05 +0300

    set different flags for compilers

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
+import platform
+import cpufeature
 from setuptools import setup, find_packages
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 __version__ = "0.1.0"
 
-extra_compile_flags = ["-march=native"]
+os_type = platform.system()
+features = cpufeature.CPUFeature
+
+if os_type == "Windows":
+    if features["AVX2"]:
+        extra_compile_flags = ["/arch:AVX2"]
+    elif features["AVX"]:
+        extra_compile_flags = ["/arch:AVX"]
+    elif features["SSE4_2"]:
+        extra_compile_flags = ["/arch:SSE4.2"]
+    elif features["SSE4_1"]:
+        extra_compile_flags = ["/arch:SSE4.1"]
+    elif features["SSSE3"]:
+        extra_compile_flags = ["/arch:SSSE3"]
+elif os_type == "Linux":
+    extra_compile_flags = ["-march=native"]
+elif os_type == "Darwin":
+    extra_compile_flags = ["-march=native"]
+
 
 ext_modules = [
     Pybind11Extension(
